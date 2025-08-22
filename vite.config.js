1d72dd4123517d74c9b2a3d8bc4ad67338275da7
Raj5636 <raj15081996.rk@gmail.com>
2025-08-22 13:31:31 +0530

    Commit changes
    Commit changes

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: './index.html'
     }
   },
   server: {
