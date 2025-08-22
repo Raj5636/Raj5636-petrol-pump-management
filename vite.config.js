460771839b4f3caa4e437b504c0a64a29b0f890b
Raj5636 <raj15081996.rk@gmail.com>
2025-08-22 13:13:36 +0530

    Commit changes
    Commit changes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   server: {
     open: true,
-  },
-  optimizeDeps: {
-    entries: 'index.html',
-  },
+  }
 })
